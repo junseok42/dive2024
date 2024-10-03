8b847f288c462e375bfcfd341294abff83cac57e
minecoby <minecoby@naver.com>
2024-10-03 23:30:44 +0900

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,9 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text
-from sqlalchemy.ext.declarative import declarative_base
+from database import user_Base, stamp_Base, region_Base
 from datetime import datetime
 
-Base = declarative_base()
-
 # User 모델 정의
-class User(Base):
+class User(user_Base):
     __tablename__ = "user_info"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -14,13 +12,13 @@ class User(Base):
     user_name = Column(String(30), nullable=False)
 
 # 지역구 테이블 정의
-class District(Base):
+class District(region_Base):
     __tablename__ = "districts"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), nullable=False) 
 
 # Stamp 테이블 정의
-class Stamp(Base):
+class Stamp(stamp_Base):
     __tablename__ = "stamps"
     id = Column(Integer, primary_key=True, index=True)
     stamp_name = Column(String(100), nullable=False)  
@@ -32,7 +30,7 @@ class Stamp(Base):
     created_at = Column(DateTime, default=datetime.utcnow)
 
 # 유저 스탬프 기록 테이블 정의
-class UserStamp(Base):
+class UserStamp(stamp_Base):
     __tablename__ = "user_stamps"
     received_at = Column(DateTime, default=datetime.utcnow)
     stamp_id = Column(Integer, ForeignKey('stamps.id'), primary_key=True)
