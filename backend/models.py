2dad9c5679d3da272a57cabcadd14bc24f79361d
minecoby <minecoby@naver.com>
2024-10-03 23:12:21 +0900

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey,Text
-from sqlalchemy.orm import relationship
-from database import user_Base, stamp_Base
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
+Base = declarative_base()
+
 # User 모델 정의
-class User(user_Base):
+class User(Base):
     __tablename__ = "user_info"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -12,19 +13,14 @@ class User(user_Base):
     password = Column(String(255), nullable=False)
     user_name = Column(String(30), nullable=False)
 
-    stamps = relationship("UserStamp", back_populates="user")
-
-#지역구 테이블 정의
-class District(user_Base):
+# 지역구 테이블 정의
+class District(Base):
     __tablename__ = "districts"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), nullable=False) 
 
-    stamps = relationship("Stamp", back_populates="district")
-
-
 # Stamp 테이블 정의
-class Stamp(stamp_Base):
+class Stamp(Base):
     __tablename__ = "stamps"
     id = Column(Integer, primary_key=True, index=True)
     stamp_name = Column(String(100), nullable=False)  
@@ -35,16 +31,9 @@ class Stamp(stamp_Base):
     district_id = Column(Integer, ForeignKey('districts.id'), nullable=False)  
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    district = relationship("District", back_populates="stamps")
-    user_stamps = relationship("UserStamp", back_populates="stamp")
-
-
 # 유저 스탬프 기록 테이블 정의
-class UserStamp(stamp_Base):
+class UserStamp(Base):
     __tablename__ = "user_stamps"
     received_at = Column(DateTime, default=datetime.utcnow)
     stamp_id = Column(Integer, ForeignKey('stamps.id'), primary_key=True)
     user_id = Column(Integer, ForeignKey('user_info.id'), primary_key=True)
-
-    user = relationship("User", back_populates="stamps")
-    stamp = relationship("Stamp", back_populates="user_stamps")
