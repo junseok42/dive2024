797204082da602872c334c5d2c8a7238aed908f2
hyojeong130 <hyojeong0109@pusan.ac.kr>
2024-10-04 14:32:22 +0900

    main_page

--- frontend/lib/view/main_page/main_page.dart
+++ frontend/lib/view/main_page/main_page.dart
@@ -6,10 +6,54 @@ class MainPageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Main Page'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('main_image.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 475),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                loginButton(),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget loginButton() {
+  return SizedBox(
+    width: 145,
+    height: 65,
+    child: OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(33.0),
+        ),
+        side: BorderSide(color: Colors.white, width: 5.0),
+        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 15.0),
+        backgroundColor: Colors.transparent,
+      ),
+      child: Text(
+        '로그인',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22.0,
+        ),
+      ),
+    ),
+  );
 }
