3fa748f3b44686aa55cd1e9281476dedb98d9548
minecoby <minecoby@naver.com>
2024-10-05 01:14:31 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,7 @@ from database import user_engine,user_Base,stamp_Base,stamp_engine,region_Base,r
 from user.user_router import router as user_router
 from stamp.stamp_router import router as stamp_router
 from region.region_router import router as region_router
+from region.house_router import router as house_router
 
 app = FastAPI()
 
@@ -33,6 +34,7 @@ region_Base.metadata.create_all(bind=region_engine)
 app.include_router(user_router, tags=["user"])
 app.include_router(stamp_router, tags=["stamp"])
 app.include_router(region_router, tags=["region"])
+app.include_router(house_router, tags=["house"])
 if __name__ == "__main__":
     import uvicorn
 
