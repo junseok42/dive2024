a9714d946dcaed038d7fd47f7899bd4db761eda9
minecoby <minecoby@naver.com>
2024-10-04 23:32:16 +0900

    스탬프장소 생성 및 유저 스탬프 확인

--- backend/stamp/stamp_router.py
+++ backend/stamp/stamp_router.py
@@ -4,10 +4,11 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 from database import get_userdb,get_stamp_db
 
-from models import User as User_model, Stamp as Stamp_model
+from models import User as User_model, Stamp as Stamp_model, UserStamp as User_Stamp_model
 
-from stamp.stamp_schema import create_stamp,stamp
+from stamp.stamp_schema import Stamp
 from user.user_func import decode_jwt
+from datetime import datetime
 security = HTTPBearer()
 
 
@@ -17,7 +18,7 @@ router = APIRouter(
 
 @router.post("/create_stamp")
 def create_new_stamp(
-                stamp: stamp,
+            stamp_data: Stamp,
               credentials: HTTPAuthorizationCredentials = Security(security), 
               user_db: Session = Depends(get_userdb),
               stamp_db: Session = Depends(get_stamp_db)):
@@ -33,5 +34,33 @@ def create_new_stamp(
         raise HTTPException(status_code=403, detail="유효하지 않은 토큰 페이로드입니다.")
     
     user_info = user_db.query(User_model).filter(User_model.user_id == user_id).one_or_none()
-    model_stamp = Stamp_model()
+    model_stamp = Stamp_model(
+        user_id = user_info.user_id,
+        title = stamp_data.title,
+        description = stamp_data.description,
+        latitude = stamp_data.latitude,
+        longitude = stamp_data.longitude,
+        district_id = stamp_data.district_id,
+        created_at = datetime.now()
+    )
+    stamp_db.add(model_stamp)
+    stamp_db.commit()
+    stamp_db.refresh(model_stamp)
+    return HTTPException(status_code=200, detail="성공적으로 스탬프장소를 추가하였습니다")
+
+@router.get("/mystamp")
+def show_my_stamp(credentials: HTTPAuthorizationCredentials = Security(security), 
+                  stamp_db: Session = Depends(get_stamp_db)):
+    token = credentials.credentials
     
+    try:
+        payload = decode_jwt(token)
+    except Exception as e:
+        raise HTTPException(status_code=403, detail="유효하지 않은 토큰입니다.")
+    
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=403, detail="유효하지 않은 토큰 페이로드입니다.")
+
+    my_stamp = stamp_db.query(User_Stamp_model).filter(User_Stamp_model.user_id == user_id).all()
+    return [{"id": mydata.stamp_id, "received" : mydata.received_at} for mydata in my_stamp]
--- backend/stamp/stamp_schema.py
+++ backend/stamp/stamp_schema.py
@@ -2,23 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-class stamp(BaseModel):
-    user_id : str
+class Stamp(BaseModel):
     title : str
     description : str
     latitude : float
     longitude : float
     district_id : int
 
-
-class create_stamp(BaseModel):
-    user_id : str
-    title : str
-    description : str
-    latitude : float
-    longitude : float
-    district_id : int
-    created_at : datetime
     
 class location_stamp(BaseModel):
     id : int
